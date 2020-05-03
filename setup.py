a9e6018c2655d7d37a931742172f797cd7807676
Enn3Developer <arturpeshko39@gmail.com>
2020-05-03 16:35:50 +0200

    Preparing to update on PyPi

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name = "NConsole",
     packages = ["NConsole"],
-    version = "1.0",
+    version = "1.1",
     license = "gpl-3.0",
     description = "This is a library to emulate a console and create commands for it",
     author = "Enn3DevPlayer",
     author_email = "arturpeshko39@gmail.com",
     url = "https://github.com/Enn3Developer/NConsole",
-    download_url = "https://github.com/Enn3Developer/NConsole/archive/1.0.tar.gz",
+    download_url = "https://github.com/Enn3Developer/NConsole/archive/1.1.tar.gz",
     keywords = ["console", "commands"],
     install_requires = [],
     classifiers = [
